502c608ba5f833f070445f09b05daf276f0775b9
Dev <Dev1@gmail.com>
2020-07-04 08:47:41 +0530

    new commit

---
+++ SpringBootRunner-JavaStyle/src/main/java/com/app/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.app.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+@Bean
+public CommandLineRunner clr()
+{
+	CommandLineRunner cl=(args)->
+	{
+		System.out.println("hello java style");
+		System.out.println(Arrays.asList(args));
+	};
+	return cl;
+}
+}
